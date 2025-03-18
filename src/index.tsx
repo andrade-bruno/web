a2811d1e9e3275120de6ce788ba1746fd9a55d66
Bruno Andrade <brunoandrade.contato@icloud.com>
2025-03-17 22:31:43 -0300

    feat: init sass setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AllRoutes } from "./routes";
+import "./globals.scss";
+import "./reset.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
